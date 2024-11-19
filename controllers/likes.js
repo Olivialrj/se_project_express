20fdf1fcc6ed35580a9b7ef8eeb5f6cb723f4e97
Olivialrj <olivialrj@hotmail.com>
2024-11-18 16:45:06 -0800

    Project 12

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,26 +1,30 @@
 const { NOT_FOUND, BAD_REQUEST, SERVER_ERROR } = require("../utils/errors");
 const ClothingItem = require("../models/clothingitem");
 
-module.exports.likeItem = (req, res) =>
+module.exports.likeItem = (req, res) => {
   const userId = req.user._id;
-const {itemId} = req.params;
+  const { itemId } = req.params;
   ClothingItem.findByIdAndUpdate(
-    itemId
+    itemId,
     { $addToSet: { likes: userId } }, // add _id to the array if it's not there yet
     { new: true }
-  ).orFail(() => {
-    const error = new Error ('Item not found');
-    error.statusCode = NOT_FOUND;
-    throw error;
-  }).then((updatedItem) => res.status(200).send({data:updatedItem})).catch((err) => {
-    console.error('Error liking item:', err);
-    if(err.name === 'CastError'){
-      return res.status(BAD_REQUEST).send({message: 'Invalid item ID'});
-    }
-    res
-    .status(err.statusCode || SERVER_ERROR)
-    .send({ message: err.message || 'An error occurred on the server' });
-});
+  )
+    .orFail(() => {
+      const error = new Error("Item not found");
+      error.statusCode = NOT_FOUND;
+      throw error;
+    })
+    .then((updatedItem) => res.status(200).send({ data: updatedItem }))
+    .catch((err) => {
+      console.error("Error liking item:", err);
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
+      }
+      res
+        .status(err.statusCode || SERVER_ERROR)
+        .send({ message: err.message || "An error occurred on the server" });
+    });
+};
 
 module.exports.dislikeItem = (req, res) => {
   const userId = req.user._id;
@@ -32,20 +36,20 @@ module.exports.dislikeItem = (req, res) => {
     { new: true }
   )
     .orFail(() => {
-      const error = new Error('Item not found');
+      const error = new Error("Item not found");
       error.statusCode = NOT_FOUND;
       throw error;
     })
     .then((updatedItem) => res.status(200).send({ data: updatedItem }))
     .catch((err) => {
-      console.error('Error disliking item:', err);
+      console.error("Error disliking item:", err);
 
-      if (err.name === 'CastError') {
-        return res.status(BAD_REQUEST).send({ message: 'Invalid item ID' });
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid item ID" });
       }
 
       res
         .status(err.statusCode || SERVER_ERROR)
-        .send({ message: err.message || 'An error occurred on the server' });
+        .send({ message: err.message || "An error occurred on the server" });
     });
 };
